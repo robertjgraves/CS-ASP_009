e5b9b2ee57bdfe111864c89fab1db75579e3ad3e
Robert Graves <robertjgraves@gmail.com>
2015-05-19 09:23:03 -0700

    Initial commit

---
+++ CS-ASP_009/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_009.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="inputTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" Text="Go" OnClick="okButton_Click" />
+        <br />
+        <br />
+        <asp:Label ID="resultsLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_009/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_009
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            int i = 5;
+            int j = 6;
+            int result = i + j;
+            // int result = i - j;
+            // int result = i * j;
+            // int result = i / j;
+
+            /* 
+            i = i + 1;
+            i += 1;
+            i++;
+            i--;
+             */
+
+            // int myInteger = (5 + 1) * 7;
+            // resultsLabel.Text = myInteger.ToString();
+
+            //double myDouble = 5.5;
+            //int myInt = 7;
+
+            //int myResult = (int)myDouble + myInt;
+
+            int firstNumber = 2000000000;
+            int secondNumber = 2000000000;
+            long resultNumber;
+
+            checked
+            {
+                resultNumber = firstNumber * secondNumber;
+            }
+
+            resultsLabel.Text = resultNumber.ToString();
+
+        }
+    }
+}
